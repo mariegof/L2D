aea95b1c6cb68fffe58e0a17beb168c3f99a52fe
mariegof <marie.goffin@gmail.com>
2025-04-19 19:06:59 +0200

    paper reproduction for 6x6 and 15x15

--- plot.py
+++ plot.py
@@ -11,12 +11,12 @@ show = True
 save = False
 save_file_type = '.pdf'
 # problem params
-n_j = 15
-n_m = 15
+n_j = 6
+n_m = 6
 l = 1
 h = 99
 stride = 50
-datatype = 'vali'  # 'vali', 'log'
+datatype = 'log'  # 'vali', 'log'
 
 
 f = open('./{}_{}_{}_{}_{}.txt'.format(datatype, n_j, n_m, l, h), 'r').readline()
@@ -35,7 +35,7 @@ if datatype == 'vali':
     if show:
         plt.show()
 elif datatype == 'log':
-    obj = numpy.array([float(s) for s in re.findall(r'-?\d+\.?\d*', f)[1::2]])[:].reshape(-1, stride).mean(axis=-1)
+    obj = -numpy.array([float(s) for s in re.findall(r'-?\d+\.?\d*', f)[1::2]])[:].reshape(-1, stride).mean(axis=-1)
     idx = np.arange(obj.shape[0])
     # plotting...
     plt.xlabel('Iteration', {'size': x_label_scale})
